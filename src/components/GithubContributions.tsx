ca51feed44d4a71dfb240690687fc23537bfc989
Harith Divarathna <harithdivarathna@gmail.com>
2026-09-08 18:25:25 +0530

    Fix react-github-calendar import to use named export

--- src/components/GithubContributions.tsx
+++ src/components/GithubContributions.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import GitHubCalendar from 'react-github-calendar';
+import { GitHubCalendar } from 'react-github-calendar';
 
 export default function GithubContributions() {
   return (
